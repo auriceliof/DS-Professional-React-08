d1699f9b7df0f35a78b7fde45b441a916ea158d9
auriceliof <auricelio.suporte@hotmail.com>
2024-03-05 13:16:09 -0300

    Formulários, CRUD completo

---
+++ dscommerce-frontend/src/services/product-service.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { BASE_URL } from "../utils/system";
+import { requestBackend } from "../utils/requests";
+
+export function findPageRequest(page: number, name: string, size = 12, sort = "name") {
+    const config : AxiosRequestConfig = {
+        method: "GET",
+        baseURL: BASE_URL,
+        url: "/products",
+        params: {
+            page,
+            name,
+            size,
+            sort
+        }
+    }    
+    
+    return requestBackend(config)
+}
+
+export function findById(id: number) {
+    return requestBackend( { url: `/products/${id}` });
+}
