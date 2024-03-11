faa840b7ce0ee1131666b8657877e4b562c1d5a1
auriceliof <auricelio.suporte@hotmail.com>
2024-03-11 16:07:10 -0300

    08-11 Layout da dialog modal de aviso

--- dscommerce-frontend/src/routes/Admin/ProductListing/index.tsx
+++ dscommerce-frontend/src/routes/Admin/ProductListing/index.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product';
 import SearchBar from '../../../components/SearchBar';
 import ButtonNextPage from '../../../components/ButtonNextPage';
+import DialogInfo from '../../../components/DialogInfo';
 
 type QueryParams = {
     page: number;
@@ -85,6 +86,9 @@ export default function ProductListing() {
                     <ButtonNextPage  onNextPage={handleNextPageClick}/>
                 }
             </section>
+
+            <DialogInfo />
+            
         </main>
     );
 }
