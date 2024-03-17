45f95cba2a6676b794846bc8fe26619a8d7efc61
auriceliof <auricelio.suporte@hotmail.com>
2024-03-17 13:25:24 -0300

    08-19 Função update do módulo forms

--- dscommerce-frontend/src/routes/ClientHome/Login/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Login/index.tsx
@@ -1,8 +1,9 @@
 import "./styles.css";
 import { useContext, useState } from "react";
-import * as authService from "../../../services/auth-service";
 import { useNavigate } from "react-router-dom";
 import { ContextToken } from "../../../utils/context-token";
+import * as authService from "../../../services/auth-service";
+import * as forms from "../../../utils/forms";
 
 export default function Login() {
   const { setContextTokenPayload } = useContext(ContextToken);
@@ -40,9 +41,7 @@ export default function Login() {
   }
 
   function handleInputChange(event: any) {
-    const value = event.target.value;
-    const name = event.target.name;
-    setFormData({ ...formData, [name]: { ...formData[name], value: value } });
+    setFormData(forms.update(formData,event.target.name, event.target.value));
   }
 
   return (
