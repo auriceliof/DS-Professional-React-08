696a00923a1c9d801b9385c0767eb7e0af905b23
auriceliof <auricelio.suporte@hotmail.com>
2024-04-07 12:17:38 -0300

    08-36 Sujando e validando tudo programaticamente

--- dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
+++ dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
@@ -100,7 +100,11 @@ export default function ProductForm() {
     function handleSubmit(event: any) {
         event.preventDefault();
 
-        console.log(forms.toValues(formData));
+        const formDataValidated = forms.dirtyAndValidateAll(formData);
+        if (forms.hasAnyInvalid(formDataValidated)) {
+            setFormData(formDataValidated);
+            return;
+        }
     }
 
     return (
