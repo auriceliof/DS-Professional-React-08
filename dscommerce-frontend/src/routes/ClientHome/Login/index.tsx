cd8e2c6dca0bcd239552f19d385b4d7691f76cf9
auriceliof <auricelio.suporte@hotmail.com>
2024-04-10 21:33:41 -0300

    08-40 Mostrando erro global no formulário de login

--- dscommerce-frontend/src/routes/ClientHome/Login/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Login/index.tsx
@@ -11,6 +11,8 @@ export default function Login() {
 
   const navigate = useNavigate();
 
+  const [submitResponseFail, setSubmitResponseFail] = useState(false);
+
   const [formData, setFormData] = useState<any>({
     username: {
       value: "",
@@ -34,14 +36,23 @@ export default function Login() {
 
   function handleSubmit(event: any) {
     event.preventDefault();
+
+    setSubmitResponseFail(false);
+
+    const formDataValidated = forms.dirtyAndValidateAll(formData);
+    if (forms.hasAnyInvalid(formDataValidated)) {
+        setFormData(formDataValidated);
+        return;
+    }
+    
     authService.loginRequest(forms.toValues(formData))
       .then((response) => {
         authService.saveAccessToken(response.data.access_token);
         setContextTokenPayload(authService.getAccessTokenPayload());
         navigate("/catalog");
       })
-      .catch((error) => {
-        console.log("Erro no login", error);
+      .catch(() => {
+        setSubmitResponseFail(true);
       });
   }
 
@@ -78,7 +89,12 @@ export default function Login() {
                 />
               </div>
             </div>
-
+            {
+              submitResponseFail &&
+                <div className="dsc-form-global-error">
+                  Usuário ou senha inválidos
+                </div>
+            }
             <div className="dsc-login-form-buttons dsc-mt20">
               <button type="submit" className="dsc-btn dsc-btn-blue">
                 Entrar
